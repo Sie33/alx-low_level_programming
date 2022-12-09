28297b6649ac36f73084f7233887b9e1afdc5cdb
sibongile magwaza <sibomagwaza18@gmail.com>
2022-12-09 01:49:24 -0800

    betty

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,18 +1,9 @@
-#!/bin/bash
 #include <stdio.h>
-
-
-
 /**
- *
- *  * main - prints the size of various types
- *
- *   * Return: 0 if exited properly, non-zero otherwise
- *
- *    */
-
+ * main - prints the size of various types
+ * Return: 0 if exited properly, non-zero otherwise
+*/
 int main(void)
-
 {
 
 		printf("Size of a char: %d byte(s)\n", sizeof(char));
@@ -26,5 +17,4 @@ int main(void)
 						printf("Size of a float: %d byte(s)\n", sizeof(float));
 
 							return (0);
-
 }
