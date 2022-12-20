491a17c93bc874410827f48de7188aeec2dd2a5a
sibongile magwaza <sibomagwaza18@gmail.com>
2022-12-20 05:12:22 +0200

    digits

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,6 +1,6 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 /* more headers goes there */
 /* betty style doc for function main goes here*/
 /**
