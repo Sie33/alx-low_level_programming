92e0832d76888111acc0c4b586bb6ef2302cdac3
sibongile magwaza <sibomagwaza18@gmail.com>
2023-01-02 09:32:38 +0200

    advanced more pointers

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,7 +11,7 @@ char *_strncpy(char *dest, char *src, int n)
 {
 int i;
 /**
-* iterate through src array
+* iterate through src array.
 * where if there is no null byte
 * among the first n bytes of source
 * the string placed in dest will not be
@@ -20,7 +20,7 @@ int i;
 for (i = 0; i < n && src[i] != '\0'; i++)
 dest[i] = src[i];
 /**
-* if the length of source is less than n
+* if the length of source is less than n.
 * write additional nullbytes to dest to
 * ensure that a total of n bytes is written
 */
