8a400d80061e3c4a6b5e3783c9f64b6ea143cce0
sibongile magwaza <sibomagwaza18@gmail.com>
2023-01-28 00:58:17 +0200

    coreections

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,20 +1,12 @@
 #include "lists.h"
 /**
-* free_list - free single list link
-*
-* @head: pointer to the first node of the list
-*
-* Return: nothing
+* free_list - frees a list
+* @head: head of the linked list.
+* Return: no return.
 */
 void free_list(list_t *head)
 {
 list_t *current;
-/**
-* while current head is not NULL
-* set head as the next node and
-* free the current node str then
-* free current node.
-*/
 while ((current = head) != NULL)
 {
 head = head->next;
@@ -22,3 +14,4 @@ free(current->str);
 free(current);
 }
 }
+
